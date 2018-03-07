6b50e9f222089147d6b9e3741a3c22cc5e6a75a0
Aikanime <owariseraph48@yahoo.com>
2018-03-07 21:22:41 +0800

    Add files via upload

---
+++ paralax.js
@@ -0,0 +1,18 @@
+//<![CDATA[
+(function (){
+  $(&#39;#particle&#39;).particleground({
+   density: 17000,
+   dotColor: &#39;#ccb679&#39;,
+   lineColor: &#39;#b58156&#39;,
+   particleRadius: 5,
+   proximity: 150,
+   parallaxMultiplier: 10,
+   parallax: false,
+   minSpeedX: 0.1,
+   minSpeedY: 0.1,
+   maxSpeedX: 1,
+   maxSpeedY: 1
+  });
+ })();
+ gNavController(&#39;#gnav&#39;);
+ //]]>
