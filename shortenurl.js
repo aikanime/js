52aa862963c5d37ddee81a7a066e93cb695e1c7d
Aikanime <owariseraph48@yahoo.com>
2018-03-07 20:50:26 +0800

    Update shortenurl.js

--- shortenurl.js
+++ shortenurl.js
@@ -1,22 +1,4 @@
-    function loadscript(filename) {
-      var scr=document.createElement('script');
-      scr.setAttribute("type","text/javascript");
-      scr.setAttribute("src",filename);
-      document.getElementsByTagName("head")[0].appendChild(scr);
-    }
-    var waitElm=null;
-    function urltinyfyprompt_callback(response) {
-      if(waitElm) waitElm.style.display="none";
-      var txt="Short URL";
-      if(response.title && response.title!="") txt+=' for "'+response.title+'"';
-      else if(response.longurl && response.longurl!="") txt+=" for "+response.longurl;
-      if(response.tinyurl && response.tinyurl!="")
-        prompt(txt+":", response.tinyurl);
-      else
-        alert("Could not get short URL, try again later." + ((response.error && response.error != "") ? ("nError: " + response.error) : ""));
-    }
-    function shortenUrl(elm, long_url, service) {
-      waitElm=elm.parentNode.nextSibling;
-      if(waitElm) waitElm.style.display="inline";
-      loadscript("https://urltinyfy.appspot.com/"+service+"?unify=1&url="+encodeURIComponent(long_url) + "&callback=urltinyfyprompt_callback");
-    }
+function downloadJSAtOnload(){var e=document.createElement("script");e.src="https://cdn.rawgit.com/KompiAjaib/FileX/master/shortenurl.js",document.body.appendChild(e)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
+// Related
+$(document).ready(function() {$('ul#related-summary li img').attr('src', function(i, src) 
+{return src.replace( 's72-c', 'w200-h135-c' );});});
