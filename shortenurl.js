e87ab549291183d88e7313f966ec8bbb5e45cf66
Aikanime <owariseraph48@yahoo.com>
2018-03-07 21:02:01 +0800

    Update shortenurl.js

--- shortenurl.js
+++ shortenurl.js
@@ -1,4 +1,6 @@
+//<![CDATA[
 function downloadJSAtOnload(){var e=document.createElement("script");e.src="https://cdn.rawgit.com/KompiAjaib/FileX/master/shortenurl.js",document.body.appendChild(e)}window.addEventListener?window.addEventListener("load",downloadJSAtOnload,!1):window.attachEvent?window.attachEvent("onload",downloadJSAtOnload):window.onload=downloadJSAtOnload;
 // Related
 $(document).ready(function() {$('ul#related-summary li img').attr('src', function(i, src) 
 {return src.replace( 's72-c', 'w200-h135-c' );});});
+//]]>
